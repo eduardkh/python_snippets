6e0d8848d5158e3d43fa83bcd430db11be7a3f23
Eduard Khiaev <eddietheone1984@gmail.com>
2022-07-28 16:00:09 +0300

    stack - raise IndexError

--- stack.py
+++ stack.py
@@ -9,19 +9,22 @@ class Stack():
         self.stack.append(value)
 
     def pop(self):
+        if self.isEmpty():
+            raise IndexError("Empty stack")
         return self.stack.pop()
 
     def __str__(self):
-        return ','.join(map(str, self.stack))
+        return f"stack: {str(self.stack)}"
 
 
 stack = Stack()
 print(f"is the stack empty: {stack.isEmpty()}")
 stack.push(1)
 stack.push(2)
+print(stack)
 stack.push(3)
 stack.push(4)
 stack.push(5)
 print(f"popping: {stack.pop()}")
 print(f"is the stack empty: {stack.isEmpty()}")
-print(f"total in stack: {stack}")
+print(stack)
