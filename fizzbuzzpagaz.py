2385231eb5e178a54d779d7d21eb7ddd5fbcf406
Eduard Khiaev <eddietheone1984@gmail.com>
2022-08-02 10:51:04 +0300

    fizzbuzzpagaz

---
+++ fizzbuzzpagaz.py
@@ -0,0 +1,16 @@
+def fizzbuzzpagaz(max):
+    for i in range(1, max+1):
+        # print(f" > i is: {i}")  # for debug
+        output = ""
+        if i % 3 != 0 and i % 5 != 0 and i % 15 != 0:
+            output = i
+        if i % 15 == 0:
+            output = f"pagaz{output}"
+        if i % 5 == 0:
+            output = f"buzz{output}"
+        if i % 3 == 0:
+            output = f"fizz{output}"
+        print(output)
+
+
+fizzbuzzpagaz(100)
